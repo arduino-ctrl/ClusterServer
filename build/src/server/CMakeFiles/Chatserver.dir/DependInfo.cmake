97541383cb545f2e4011d55187bc2ec1d8474d41
arduino-ctrl <3259416392@qq.com>
2025-02-17 23:53:17 +0800

    Save changes before pull

---
+++ build/src/server/CMakeFiles/Chatserver.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/chatserver.cpp" "src/server/CMakeFiles/Chatserver.dir/chatserver.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/chatserver.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/chatservice.cpp" "src/server/CMakeFiles/Chatserver.dir/chatservice.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/chatservice.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/db/db.cpp" "src/server/CMakeFiles/Chatserver.dir/db/db.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/db/db.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/main.cpp" "src/server/CMakeFiles/Chatserver.dir/main.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/main.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/model/friendmodel.cpp" "src/server/CMakeFiles/Chatserver.dir/model/friendmodel.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/model/friendmodel.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/model/groupmodel.cpp" "src/server/CMakeFiles/Chatserver.dir/model/groupmodel.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/model/groupmodel.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/model/offlinemessagemodel.cpp" "src/server/CMakeFiles/Chatserver.dir/model/offlinemessagemodel.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/model/offlinemessagemodel.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/model/usermodel.cpp" "src/server/CMakeFiles/Chatserver.dir/model/usermodel.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/model/usermodel.cpp.o.d"
+  "/home/yangzw/Desktop/CppProject/ClusterServer/src/server/redis/redis.cpp" "src/server/CMakeFiles/Chatserver.dir/redis/redis.cpp.o" "gcc" "src/server/CMakeFiles/Chatserver.dir/redis/redis.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
